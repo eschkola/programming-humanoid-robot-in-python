4f046e4fbdfe4f7d87dd4528782e24d5fa466728
eschkola <e.schulenburg@campus.tu-berlin.de>
2021-05-16 15:10:11 +0200

    alles so halb working #

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -22,6 +22,10 @@
 
 from pid import PIDAgent
 from keyframes import hello
+from keyframes import rightBackToStand
+from scipy import interpolate
+import numpy as np
+from scipy.interpolate import BSpline
 
 
 class AngleInterpolationAgent(PIDAgent):
@@ -32,6 +36,7 @@ class AngleInterpolationAgent(PIDAgent):
                  sync_mode=True):
         super(AngleInterpolationAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.keyframes = ([], [], [])
+        self.start_time = -1
 
     def think(self, perception):
         target_joints = self.angle_interpolation(self.keyframes, perception)
@@ -41,10 +46,79 @@ class AngleInterpolationAgent(PIDAgent):
     def angle_interpolation(self, keyframes, perception):
         target_joints = {}
         # YOUR CODE HERE
+        
+        #__________________________________________________________________
+        
+        all_names, all_times, all_keys = keyframes
+        
+        if(self.start_time == -1):
+            self.start_time = perception.time
+            
+        actual_time = perception.time - self.start_time
+        
+        
+        for joint in range(len(all_names)):
+            name = all_names[joint]
+            
+            if name not in self.joint_names:
+                continue
+                        
+            joint_times = all_times[joint]
+            joint_keys = all_keys[joint]
+           
+            
+            last_elem = len(joint_times)
+            if(actual_time >= (joint_times[0] - 0.1) and actual_time <= joint_times[last_elem-1]):
+                f = interpolate.splrep(joint_times, [item[0] for item in joint_keys])
+                target_joints[name] = interpolate.splev(actual_time, f)
+        
+        #__________________________________________________________________
+        
+        # names, times, keys = keyframes
 
+        # if self.start_time == 0:
+        #     self.start_time = perception.time
+
+        # new_time = perception.time - self.start_time
+
+        # for i in range(len(names)):
+        #     name = names[i]
+
+        #     if name not in self.joint_names:
+        #         continue
+
+        #     time = times[i]
+
+        #     key = keys[i]
+
+        #     for t in range(len(time)):
+        #         if new_time > time[-1]:
+        #             continue
+
+        #         if new_time < time[t]:
+
+        #             if t == 0:
+        #                 T0, P0, P1 = 0, 0, 0
+        #                 T3 = time[0]
+        #                 P2 = key[0][1][2]
+        #                 P3 = key[0][0]
+        #                 j = new_time / T3
+                    
+        #             elif new_time >= time[t - 1]:
+        #                 T0 = time[t - 1]
+        #                 T3 = time[t]
+        #                 P0 = key[t - 1][0]
+        #                 P3 = key[t][0]
+        #                 P1 = P0 + key[t - 1][2][2]
+        #                 P2 = P3 + key[t][1][2]
+        #                 j = (new_time - T0)/(T3 - T0)
+
+        #             target_joints[name] = ((1 - j) ** 3) * P0 + 3 * ((1 - j) ** 2) * j * P1 + 3 * ((1 - j) ** 3) * (j **2) * P2 + (j ** 3) * P3
+
+        
         return target_joints
 
 if __name__ == '__main__':
     agent = AngleInterpolationAgent()
-    agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
+    agent.keyframes = rightBackToStand()  # CHANGE DIFFERENT KEYFRAMES
     agent.run()
