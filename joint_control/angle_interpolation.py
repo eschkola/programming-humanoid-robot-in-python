0e2c28bb3c573c6cf3333001a477d9bc07ab9ff3
eschkola <e.schulenburg@campus.tu-berlin.de>
2021-05-18 21:52:19 +0200

    homework 2 - spline has bug with second leg - not my fault

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -22,10 +22,11 @@
 
 from pid import PIDAgent
 from keyframes import hello
-from keyframes import rightBackToStand
+from keyframes import leftBackToStand
 from scipy import interpolate
 import numpy as np
 from scipy.interpolate import BSpline
+import standing_up
 
 
 class AngleInterpolationAgent(PIDAgent):
@@ -51,31 +52,80 @@ class AngleInterpolationAgent(PIDAgent):
         
         all_names, all_times, all_keys = keyframes
         
-        if(self.start_time == -1):
+        if(self.start_time < 0):
             self.start_time = perception.time
             
         actual_time = perception.time - self.start_time
         
-        
         for joint in range(len(all_names)):
-            name = all_names[joint]
-            
-            if name not in self.joint_names:
-                continue
-                        
+                
+            name = all_names[joint]    
             joint_times = all_times[joint]
             joint_keys = all_keys[joint]
-           
             
-            last_elem = len(joint_times)
-            if(actual_time >= (joint_times[0] - 0.1) and actual_time <= joint_times[last_elem-1]):
-                f = interpolate.splrep(joint_times, [item[0] for item in joint_keys])
+            first_items = [item[0] for item in joint_keys]
+
+                        
+            if actual_time > joint_times[-1]:
+                target_joints[name] = first_items[-1]
+                if joint == len(all_names) - 1:
+                    self.keyframes = ([], [], [])
+                    self.start_times = -1;
+                    
+            if actual_time <= joint_times[0]:
+                target_joints[name] = first_items[0]
+                
+            
+
+            if(joint_times[0] <= actual_time <= joint_times[-1]):
+                
+                # original idea: spline with scipy
+                f = interpolate.splrep(joint_times, first_items)
                 target_joints[name] = interpolate.splev(actual_time, f)
                 
+                # bezier - should be working but not my original idea
+                # frame_n = np.argmax(np.array(joint_times) > actual_time)
+                # max_t = joint_times[frame_n]
+                # min_t = joint_times[frame_n-1] if frame_n != 0 else 0
+                # time = (actual_time - min_t) / (max_t - min_t)
+                # target_joints[name] = self.bezier(joint_keys, frame_n, time)
+                
+                # point = np.argmax(np.array(joint_times) > actual_time)
+                
+                # P3 = joint_keys[point][0]
+                # P0 = joint_keys[point - 1][0]
+                # P1 = P0 + joint_keys[point - 1][2][2]
+                # P2 = P3 + joint_keys[point][1][2]
+                
+                # if point != 0:
+                #     min = joint_times[point - 1]
+                # else:
+                #     min = 0
+                
+                # max = joint_times[point]
+                
+                # t =  (actual_time - min) / (max - min)
+                
+                # target_joints[name] = ((1-t) ** 3) * P0 \
+                #                         + 3 * (1-t) ** 2 * t * P1 \
+                #                         + 3 * (1-t) * (t ** 2) * P2 \
+                #                         + t ** 3 * P3
+                
+            if ("LHipYawPitch" in target_joints):
+                target_joints["RHipYawPitch"] = target_joints["LHipYawPitch"]
+       
         
         return target_joints
+    
+    def bezier(self, keys, frame_n, t):
+        p0 = keys[frame_n-1][0]
+        p3 = keys[frame_n][0]
+        p1 = p0 + keys[frame_n-1][2][2]
+        p2 = p3 + keys[frame_n][1][2]
+        return np.power(1 - t, 3) * p0 + 3 * t * np.power(1 - t, 2) * p1 + 3 * np.power(t, 2) * (
+                    1 - t) * p2 + np.power(t, 3) * p3
 
 if __name__ == '__main__':
     agent = AngleInterpolationAgent()
-    agent.keyframes = rightBackToStand()  # CHANGE DIFFERENT KEYFRAMES
+    agent.keyframes = leftBackToStand()  # CHANGE DIFFERENT KEYFRAMES
     agent.run()
